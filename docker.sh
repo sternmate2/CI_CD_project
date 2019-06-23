535cf4da1fee8ce1b0bf6455ed3f4956ac0f1b21
sternmate2 <shaharstern@hotmail.com>
2019-06-23 18:49:22 +0300

    docker container build

--- docker.sh
+++ docker.sh
@@ -3,6 +3,6 @@
 #sudo docker cp ~/ci_cd/index.html WebSrv:/usr/local/apache2/htdocs/ &&
 #sudo docker cp ~/ci_cd/css WebSrv:/usr/local/apache2/htdocs/css &&
 #sudo docker cp ~/ci_cd/images WebSrv:/usr/local/apache2/htdocs/images 
-docker build -t . &&
-docker run -d httpd:2.4 
+sudo docker build -t . &&
+sudo docker run -d httpd:2.4 
 
