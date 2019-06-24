68c14bfe910199ced1b21f4fb8593d1cf4a69830
sternmate2 <shaharstern@hotmail.com>
2019-06-24 20:42:49 +0300

    test

--- docker.sh
+++ docker.sh
@@ -2,7 +2,9 @@
 
 rsync -avz ./docker.sh shahar@192.168.1.109:/var/www//html/docker.sh &&
 
-ssh shahar@192.168.1.109 "cd /var/www/html/ ; docker build -t apache2 ."
+ssh shahar@192.168.1.109 "cd /var/www/html/ ; docker build -t apache2 ." &&
+
+ssh shahar@192.168.1.109 "docker run -dit --name my-running-app -p 8080:80 httpd:2.4"
 
 
 
